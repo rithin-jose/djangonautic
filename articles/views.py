49c22d3de2367998b6cdb95a0c46fa5d819e03c2
Rithin Jose <rithinja@gmail.com>
2019-10-26 11:48:26 +0530

    model method update

--- articles/views.py
+++ articles/views.py
@@ -3,5 +3,5 @@ from .models import Article
 
 # Create your views here.
 def article_list(request):
-    articles = Article.object.all().order_by('date') 
+    articles = Article.objects.all().order_by('date') 
     return render(request, 'articles/article_list.html',{'articles':articles})
